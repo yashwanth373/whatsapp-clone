1d32c411d364c4c8607f5e197981b6a3cdb7b6c7
Yashwanth <jyashwanthreddy373@gmail.com>
2021-05-30 23:24:57 +0530

    Message sending and DropDown Menu accomplished

--- src/components/ChatSection.js
+++ src/components/ChatSection.js
@@ -1,31 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { AiOutlineSearch, AiOutlinePaperClip } from "react-icons/ai";
 import { BiDotsVerticalRounded } from "react-icons/bi";
 import { CgSmileMouthOpen } from "react-icons/cg";
 import { MdMic } from "react-icons/md";
+import { FiChevronDown } from "react-icons/fi";
 export default function ChatSection() {
-  const [group, setGroup] = useState({
+  let dummyUserId = 2750192;
+  const [group] = useState({
     id: "",
     name: "Dance Group",
     img: "",
     users: [],
     createdAt: "",
   });
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([
+    {
+      id: Date.now(),
+      senderId: 2323245,
+      message: "Hello World",
+      time: "14:52,30/5/2021",
+    },
+  ]);
+
   function sendMessage(message) {
-    console.log(message);
-    setMessages([...messages,message])
+    setMessages([...messages, message]);
   }
   return (
     <div className="chat">
       <TopBar name={group.name} users={group.users} img={group.img} />
-      <MessageSection messages={messages}/>
-      <SendBar sendMessage={sendMessage} />
+      <MessageSection messages={messages} dummyUserId={dummyUserId} />
+      <SendBar sendMessage={sendMessage} dummyUserId={dummyUserId} />
     </div>
   );
 }
 
 function TopBar({ name, users, img }) {
+  const [ddtoggle, setddtoggle] = useState(false);
+  const wref = useRef(null);
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setddtoggle(!ddtoggle)
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref,ddtoggle]);
+  }
+  useOutsideAlerter(wref);
+  function DropDown() {
+    return (
+      <div ref={wref} className="ddMenu">
+        <span className="ddItem">Contact info</span>
+        <span className="ddItem">Select Messages</span>
+        <span className="ddItem">Mute Notifications</span>
+        <span className="ddItem">Delete Chat</span>
+      </div>
+    );
+  }
   return (
     <div className="topBar">
       <div onClick={() => alert("dp clicked")}>
@@ -52,24 +87,85 @@ function TopBar({ name, users, img }) {
       </div>
       <div className="icons">
         <AiOutlineSearch onClick={() => alert("Search Clicked")} />
-        <BiDotsVerticalRounded onClick={() => alert("Options Clicked")} />
+        <BiDotsVerticalRounded
+          role="button"
+          onClick={() => setddtoggle(!ddtoggle)}
+        />
+      </div>
+      {ddtoggle && <DropDown />}
+    </div>
+  );
+}
+
+function MessageSection({ messages, dummyUserId }) {
+  const Sent = ({ message }) => {
+    return (
+      <div className="sent">
+        <button className="dd">
+          <FiChevronDown className="ddIcon" />
+        </button>
+        <span className="msg">{message.message}</span>
+        <span className="time">{message.time.split(",")[0]}</span>
+      </div>
+    );
+  };
+  const Received = ({ message }) => {
+    return (
+      <div className="received">
+        <button className="dd">
+          <FiChevronDown className="ddIcon" />
+        </button>
+        <span className="msg">{message.message}</span>
+        <span className="time">{message.time.split(",")[0]}</span>
+      </div>
+    );
+  };
+  return (
+    <div className="messageSection">
+      <div className="message">
+        {messages.map((message) =>
+          message.senderId === dummyUserId ? (
+            <Sent message={message} />
+          ) : (
+            <Received message={message} />
+          )
+        )}
       </div>
     </div>
   );
 }
 
-function MessageSection() {
-  return <div className="messageSection">
-    
-  </div>;
-}
-
-function SendBar({ sendMessage }) {
-  const [message, setMessage] = useState("");
+function SendBar({ sendMessage, dummyUserId }) {
+  const [message, setMessage] = useState({
+    id: "",
+    senderId: null,
+    message: "",
+    time: "",
+  });
   function handleSubmit(e) {
-    console.log(message);
-    sendMessage(message);
-    setMessage("");
+    let date = new Date();
+    sendMessage({
+      ...message,
+      id: Date.now(),
+      message: message.message,
+      senderId: dummyUserId,
+      time:
+        date.getHours() +
+        ":" +
+        ((date.getMinutes() < 10 ? "0" : "") + date.getMinutes()) +
+        "," +
+        date.getDate() +
+        "/" +
+        (date.getMonth() + 1) +
+        "/" +
+        date.getFullYear(),
+    });
+    setMessage({
+      id: "",
+      senderId: "",
+      message: "",
+      time: "",
+    });
     e.preventDefault();
     // setMessage(e.target.value)
   }
@@ -90,9 +186,11 @@ function SendBar({ sendMessage }) {
             type="text"
             name="message"
             className="messageField"
-            value={message}
+            value={message.message}
             placeholder="Type a message"
-            onChange={(e) => setMessage(e.target.value)}
+            onChange={(e) =>
+              setMessage({ ...message, message: e.target.value })
+            }
           />
         </form>
       </div>
